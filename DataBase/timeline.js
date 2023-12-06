ae5e5ef9b3496f93f10ce379f7ca042ce319a568
Samuel <samuelhui2004@gmail.com>
2023-12-05 23:09:41 -0800

    finished popup feature of timeline read more

--- DataBase/timeline.js
+++ DataBase/timeline.js
@@ -4,35 +4,35 @@ export const eventTable = [
         name: 'Pre-colonial Period:',
         description: 'Neolithic Era: Evidence of human activity in the region.',
         date: 'Neolithic Era',
-        details: ''
+        details: 'The Neolithic Era, also known as the New Stone Age, was a period characterized by the development of agriculture, the domestication of animals, and the use of polished stone tools. In the context of Hong Kong and the surrounding regions, evidence of human activity during the Neolithic Era provides insights into the early history of the area.'
     },
     {
         id: 'a4043688-604b-4886-af45-7989e1bd5c99',
         name: 'Pre-colonial Period',
         description: 'Chinese Imperial authorities establish a presence in the area.',
         date: '206 BCE - 220 CE',
-        details: ''
+        details: 'The establishment of Chinese imperial authority in the Hong Kong region goes back to ancient times, and several dynasties played a role in asserting control and influence over the area. The specific details may vary across different periods.'
     },
     {
         id: '708449ea-3dc2-40b3-9e14-c5d2ed1789bf',
         name: 'Pre-colonial Period',
         description: 'Hong Kong becomes a significant trading center.',
         date: '618CE - 907CE',
-        details: ''
+        details: ` During the Tang Dynasty, China experienced a golden age of cultural, economic, and political development. While there is limited direct historical evidence specifically detailing Hong Kong's role as a trading center during this period. `
     },
     {
         id: 'a69abcf2-390b-49a4-a8d8-f1f0949d9372',
         name: 'Imperial China',
         description: 'The Treaty of Nanking concludes the First Opium War, ceding Hong Kong Island to Britain.',
         date: '1842',
-        details: ''
+        details: 'The Treaty of Nanking, signed on August 29, 1842, marked the end of the First Opium War (1839–1842) between Great Britain and the Qing Dynasty of China. The treaty was negotiated following the military successes of the British forces, particularly the Royal Navy.'
     },
     {
         id: '7132b0c4-9333-4cb0-8a78-147f1c9bfd3e',
         name: 'Imperial China',
         description: 'The Convention of Peking extends British control to the Kowloon Peninsula.',
         date: '1860',
-        details: ''
+        details: 'The Convention of Peking, also known as the Second Convention of Peking, was signed on October 18, 1860, between the United Kingdom and the Qing Dynasty of China. It was a follow-up agreement to the Treaty of Nanking (1842) and addressed additional issues arising from conflicts between the two powers. One of the key provisions of the Convention of Peking was the extension of British control to the Kowloon Peninsula.'
     },
     {
         id: 'cb3ad464-d0ef-472e-8a4b-742bf18764cd',
@@ -46,153 +46,195 @@ export const eventTable = [
         name: 'Japanese Occupation',
         description: 'Hong Kong is occupied by Japan during World War II.',
         date: '1941 - 1945',
-        details: ''
+        details: 'The occupation of Hong Kong by Japan during World War II took place from December 25, 1941, when the Japanese forces invaded the British colony, to August 15, 1945, when Japan officially surrendered.'
     },
     {
         id: 'fe97352e-fab4-4d5a-960d-ae2b691e6c30',
         name: 'Post-War Period',
         description: 'Hong Kong experiences an influx of immigrants, mainly from Mainland China',
         date: '1945 - 1950s',
-        details: ''
+        details: 'After the end of World War II in 1945, Hong Kong experienced a significant influx of immigrants, mainly from Mainland China. This period of mass migration was driven by various factors, including political, economic, and social conditions. '
     },
     {
         id: '6e8a633e-5999-4c16-a380-5c61ac2fc218',
         name: 'Post-War Period',
         description: 'Economic growth accelerates, and the population continues to increase.',
         date: '1950s - 1960s',
-        details: ''
+        details: `During the 1950s and 1960s, Hong Kong experienced rapid economic growth and a significant increase in population. This period marked a crucial phase in the city's transformation into a major economic and industrial center.`
     },
     {
         id: 'b4986c9d-ee36-4fdf-8521-89fa8a53a9fb',
         name: 'Riots',
         description: 'Protests and riots, known as the 1967 Leftist Riots, occur in response to social and economic issues.',
         date: '1967s',
-        details: ''
+        details: 'The 1967 Leftist Riots in Hong Kong were a series of protests and demonstrations that unfolded against the backdrop of social, economic, and political issues. The events were characterized by a wave of labor strikes, street protests, and violent clashes between demonstrators and the authorities.'
     },
     {
         id: '121b0514-6115-4edc-ade3-0f61b1956535',
         name: 'Industrialization',
         description: "Hong Kong's industrialization continues, transforming it into a major economic hub.",
         date: '1970s',
-        details: ''
+        details: `The 1970s marked a continuation of Hong Kong's industrialization and economic development, solidifying its position as a major economic hub in the region. During this decade, the city experienced significant growth in various sectors, and its economic landscape underwent further diversification.`
     },
     {
         id: '6884686a-2793-4369-90f6-97b4b3a278ce',
         name: 'Future of Hong Kong',
         description: "Britain and China begin talks on the future of Hong Kong.",
         date: '1982',
-        details: ''
+        details: 'The talks between Britain and China on the future of Hong Kong in 1982 were a significant milestone in the diplomatic process that ultimately led to the Sino-British Joint Declaration. This agreement set the framework for the handover of Hong Kong from British to Chinese sovereignty.'
     },
     {
         id: '472cf8b2-f545-423e-ab75-e4315d7cc838',
         name: '"One Country, Two Systems"',
         description: `The Sino-British Joint Declaration is signed, outlining the "One Country, Two Systems" principle for Hong Kong's return to China in 1997.`,
         date: '1984',
-        details: ''
+        details: `The signing of the Sino-British Joint Declaration in 1984 was a pivotal moment in the history of Hong Kong, outlining the principles for the city's return to Chinese sovereignty in 1997.  `
     },
     {
         id: 'd73b411a-3e68-428e-93e7-2eed7024af36',
         name: 'Tiananmen Square massacre',
         description: `The Tiananmen Square massacre leads to calls for democratic safeguards in Hong Kong.`,
         date: '1989',
-        details: ''
+        details: 'The Tiananmen Square massacre and its aftermath played a crucial role in shaping the political climate in Hong Kong as it approached the 1997 handover. The events fueled a sense of uncertainty about the future and a determination among some Hong Kong residents to advocate for democratic values and safeguards.'
     },
     {
         id: '240dc84b-5dac-45af-9779-4ccd7c6430f9',
         name: 'Basic Law',
         description: `Beijing formally ratifies Hong Kong's post-handover mini-constitution or Basic Law.`,
         date: '1990',
-        details: ''
+        details: `In 1990, Beijing formally ratified Hong Kong's post-handover mini-constitution known as the Basic Law. This legal document would serve as the constitutional foundation for the Hong Kong Special Administrative Region (SAR) after the transfer of sovereignty from Britain to China in 1997. `
     },
     {
         id: '6f216df8-0445-4f95-a650-3df9b1b28546',
         name: 'last British governor',
         description: `Chris Patten becomes the last British governor of Hong Kong.`,
         date: '1992',
-        details: ''
+        details: `Chris Patten's tenure as the last British governor of Hong Kong was a period of significant political and historical importance, characterized by efforts to introduce democratic reforms and manage the complexities of the handover to Chinese rule. His approach to governance in Hong Kong and his advocacy for democratic principles left a lasting impact on the city's political landscape.`
     },
     {
         id: '79c955f1-d73a-4bbf-8a0e-d9a68d0ddf3e',
         name: 'Handover',
         description: `Hong Kong is handed back to China. Tung Chee-hwa is appointed as the first Chief Executive.`,
         date: '1997',
-        details: ''
+        details: 'The handover of Hong Kong in 1997 was a historic moment that signified the end of more than 150 years of British colonial rule and the beginning of a new chapter under Chinese sovereignty. It set the stage for the implementation of the "one country, two systems" framework and marked a complex transition for Hong Kong and its residents.'
     },
     {
         id: '248e2c21-1493-4c2f-a8fe-2f1ec57ebcac',
         name: 'First election',
         description: `The first post-handover elections are held.`,
         date: '1998',
-        details: ''
+        details: `The 1998 elections were a crucial step in Hong Kong's political development after the handover. They demonstrated the coexistence of different political forces and marked the beginning of the SAR's engagement with its unique political system under Chinese sovereignty. The outcomes set the stage for subsequent elections and the ongoing political evolution of Hong Kong.`
     },
     {
         id: '6db036a3-3244-4456-aa32-fab92a4fef18',
         name: 'Resign',
         description: `Anson Chan resigns under pressure from Beijing, replaced by Donald Tsang.`,
         date: '2001',
-        details: ''
+        details: `Anson Chan's resignation in 2001 was a notable event that drew attention to the evolving relationship between Hong Kong and mainland China. It reflected broader discussions about the protection of Hong Kong's autonomy and the challenges faced by individuals who sought to uphold the city's core values in the post-handover era.`
     },  
     {
         id: '01e7eb2a-240a-4f45-9a58-8721f8410935',
         name: 'Democratic reform',
         description: `Chris Patten's democratic reform proposals face opposition.`,
         date: '2002',
-        details: ''
+        details: `In 2002, Chris Patten, the last British governor of Hong Kong, faced opposition to his proposed democratic reforms in the territory. Patten served as the governor from 1992 to 1997, overseeing the transition of Hong Kong from British rule to Chinese sovereignty. During his tenure, he introduced a series of political reforms aimed at increasing the level of democracy in Hong Kong, which was met with resistance from some quarters.`
     },  
     {
         id: '727ebe43-dc96-419f-aff8-cf8ce8d5913e',
         name: 'Article 23',
-        description: `Protests against Article 23 (anti-subversion law); Hong Kong is hit by the SARS epidemic.`,
+        description: `Protests against Article 23 (anti-subversion law)Hong Kong is hit by the SARS epidemic.`,
         date: '2003',
-        details: ''
+        details: `Tung Chee-hwa's administration that the government proposed controversial national security legislation known as Article 23, which aimed to prohibit acts of treason, secession, sedition, and subversion against the Chinese government. This proposal faced significant public opposition and ultimately led to the large-scale protests in 2003, eventually shelved the proposed legislation, and Article 23 was not implemented at that time due to overwhelming public opposition,.`
     },  
+    {
+        id: 'ff11bb28-e3f8-4dd1-bebf-430000d60171',
+        name: 'SARS',
+        description: `Hong Kong is hit by the SARS epidemic.`,
+        date: '2003',
+        details: `The SARS epidemic, caused by the Severe Acute Respiratory Syndrome coronavirus (SARS-CoV), emerged in late 2002 in Guangdong Province, China. It quickly spread to Hong Kong and other parts of the world.`
+    },  
+
     {
         id: 'f82cc008-1bfe-4ccc-b5aa-be739f00701e',
         name: 'Promise for universal sufferage',
         description: `Beijing promises universal suffrage for Hong Kong's Chief Executive and Legislative Council elections.`,
         date: '2007',
-        details: ''
+        details: 'The promise for universal suffrage in 2007 set the stage for ongoing discussions and debates over the democratic development of Hong Kong. However, the actual realization of universal suffrage faced challenges and controversies in the years that followed, leading to significant political tensions and protests in the region.'
     },  
     {
         id: '233a57b9-51d5-4a57-8452-0987952d9d50',
         name: 'Umbrella Movement',
         description: `Umbrella Movement protests demand genuine universal suffrage.`,
         date: '2014',
-        details: ''
+        details: `The Umbrella Movement protests in 2014 were a series of pro-democracy demonstrations in Hong Kong that primarily demanded genuine universal suffrage in the election of the Chief Executive and the Legislative Council. The movement's name was derived from the use of umbrellas by protesters to shield themselves from tear gas and pepper spray deployed by the police.`
     },
     {
         id: 'aa13565d-b61e-4455-876d-55e5ac990742',
         name: 'Carrie Lam',
         description: `Carrie Lam becomes Chief Executive after winning the Electoral College.`,
         date: '2017',
-        details: ''
+        details: `Carrie Lam's tenure as Chief Executive has been marked by challenges and controversies, reflecting the complex political dynamics in Hong Kong. The issues raised during the Chief Executive election in 2017 continued to shape the political landscape in the subsequent years. Her term began amidst a politically charged atmosphere, with ongoing debates about democratic reforms, concerns about the erosion of freedoms, and the relationship between Hong Kong and mainland China.`
     },
     {
         id: '60688d87-6aab-4cc0-825c-6751fa61f745',
         name: 'Anti-extradition bill protest',
         description: `Anti-extradition bill protests erupt, evolving into a pro-democracy movement.`,
         date: '2019',
-        details: ''
+        details: `The anti-extradition bill protests that erupted in 2019 in Hong Kong were a series of mass demonstrations initially triggered by concerns over a proposed extradition bill. The protests quickly evolved into a broader pro-democracy movement, reflecting deep-seated concerns about the preservation of Hong Kong's autonomy and civil liberties.`
     },
     {
         id: '9ca7efa5-58e9-4cc8-b93d-ea64cf2222d7',
         name: 'National security law',
         description: `National security law imposed by China; COVID-19 pandemic affects global events.`,
         date: '2020',
-        details: ''
+        details: 'The National Security Law results in increased political censorship, arrests of pro-democracy activists, and a chilling effect on dissent. Many pro-democracy figures and activists, including media tycoon Jimmy Lai, are arrested under the new law. The law also prompts several countries to reconsider their relations with Hong Kong and China.'
+    },
+    {
+        id: 'da747ca9-6c45-4c43-b5b4-81f81c488b4b',
+        name: 'COVID-19',
+        description: `COVID-19 pandemic affects global events.`,
+        date: '2020',
+        details: ` The COVID-19 pandemic affects Hong Kong's economy, tourism, and daily life. The city implements measures such as social distancing, mask mandates, and travel restrictions. The pandemic also intersects with the political situation, influencing the dynamics of protests and government actions.`
     },
     {
         id: '14a5ad6a-204d-407e-80dd-35a368d839d7',
         name: 'Tightening',
         description: `Continued arrests under the national security law; electoral reforms tighten Beijing's control.`,
         date: '2021',
-        details: ''
+        details: 'Throughout the year, there were reports of arrests of pro-democracy activists, journalists, and individuals critical of the Beijing government, often on charges related to national security, such as secession, subversion, terrorism, or collusion with foreign forces.'
+    },
+    {
+        id: '14382992-8166-4069-ac41-95d6b527df57',
+        name: 'Reform',
+        description: `Electoral reforms tighten Beijing's control.`,
+        date: '2021',
+        details: `China's top legislature approved a major overhaul of Hong Kong's electoral system.
+        The reforms aimed to ensure that only "patriots" could govern Hong Kong, as defined by the Chinese Communist Party. Candidates would be subject to a screening process to assess their loyalty to Beijing.
+        The changes included reducing the number of directly elected seats in the Legislative Council (LegCo) and expanding the Election Committee that selects the Chief Executive.
+        Critics argued that these reforms undermined the democratic elements of Hong Kong's political system and concentrated power in the hands of those aligned with Beijing.`
     },
     {
         id: 'e82f0352-62d2-41f2-9e92-81ad7668a568',
-        name: 'No re-election',
+        name: 'Carrie Lam leaving',
         description: `Carrie Lam announces she will not seek re-election.`,
         date: '2022',
-        details: ''
+        details: `As the chief executive, Ms. Lam presided over a tumultuous period marked by extensive pro-democracy demonstrations that resulted in increased Chinese influence in Hong Kong.
+
+        Ms. Lam, aged 64, was personally selected by Beijing when she assumed office in 2017.
+        
+        In a statement to the press on Monday, she indicated that Beijing had been amenable to her decision. Ms. Lam also disclosed that she had conveyed her decision not to pursue a second term to China a year ago, despite consistently avoiding questions about her political future in recent times.
+        
+        Citing a focus on her family, Ms. Lam announced her decision to step back, stating, "There's only one consideration, and that is family... They believe it's time for me to return home."
+        
+        Hong Kong's Chief Secretary, John Lee, is reportedly the frontrunner to succeed Ms. Lam.`
     },
 ]
+
+export function getEventById(id){
+    for(let event of eventTable){
+        if(event.id == id){
+            return event;
+        }
+    }
+    return null;
+}
--- Scripts/timelineHandler.js
+++ Scripts/timelineHandler.js
@@ -1,4 +1,4 @@
-import {eventTable} from "../DataBase/timeline.js"
+import {eventTable, getEventById} from "../DataBase/timeline.js"
 
 let timelineList = document.querySelector(".js-timelineList")
 
@@ -12,10 +12,47 @@ for(let i = 0; i < eventTable.length; i++){
             <p>${item.description}</p>
             <span class="date">${item.date}</span>
             <span class="circle"></span>
-            <button class = "readMore">read more</button>
+            <button class = "readMore" data-item-id = "${item.id}">read more</button>
         </li>
     `;
 
     timelineList.innerHTML += templateBox;
+    
+}
+
+let readMoreButtons = document.querySelectorAll(".readMore")
+let overlay = document.querySelector(".overlay");
+let popupWindow = document.querySelector("#popupInfoWindow");
+let closebtn = document.getElementById("closeButton") ;
+
+for(let moreBtn of readMoreButtons){
+    moreBtn.addEventListener("click", function(){
+        overlay.style.display = "block";
+        console.log(moreBtn.dataset.itemId);
+        let item = getEventById(moreBtn.dataset.itemId);
+
+        updatePopupWindow(item.name, item.details);
+        
+        popupWindow.classList.add("popupTweenIn");
+    })
+}
+
+
+
+closebtn.addEventListener("click", function(){
+    overlay.classList.remove("overlayTween");
+    overlay.style.display = "none"
+    
+    popupWindow.classList.remove("popupTweenIn");
+ 
+});
+
+
+
+function updatePopupWindow(name, details){
+    popupWindow.querySelector("h3").innerHTML = name;
+
+    popupWindow.querySelector("p").innerHTML = details;
+
     
 }
